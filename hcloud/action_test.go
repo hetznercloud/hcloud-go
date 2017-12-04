1c96fa27a854bfdc12d36efc55e5ee22a4ca0f1f
Thomas Cyron <thomas@thcyron.de>
2017-12-04 15:01:35 +0100

    Test getting an action

--- hcloud/action_test.go
+++ hcloud/action_test.go
@@ -1,7 +1,9 @@
 package hcloud
 
 import (
+	"context"
 	"encoding/json"
+	"net/http"
 	"testing"
 	"time"
 
@@ -57,5 +59,34 @@ func TestActionFromSchema(t *testing.T) {
 	}
 	if action.ErrorMessage != "Action failed" {
 		t.Errorf("unexpected error message: %v", action.ErrorMessage)
+	}
+}
+
+func TestActionClientGet(t *testing.T) {
+	env := newTestEnv()
+	defer env.Teardown()
+
+	env.Mux.HandleFunc("/actions/1", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(schema.ActionGetResponse{
+			Action: schema.Action{
+				ID:       1,
+				Status:   "running",
+				Command:  "create_server",
+				Progress: 50,
+				Started:  time.Date(2017, 12, 4, 14, 31, 1, 0, time.UTC),
+			},
+		})
+	})
+
+	ctx := context.Background()
+	action, _, err := env.Client.Action.Get(ctx, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if action == nil {
+		t.Fatal("no action")
+	}
+	if action.ID != 1 {
+		t.Errorf("unexpected action ID: %v", action.ID)
 	}
 }
