a9796298be172f3574ed8e0e36839ebec9237d4f
Nico Schieder <nico.schieder@hetzner-cloud.de>
2018-01-09 13:16:12 +0100

    Expose API error details

--- hcloud/error.go
+++ hcloud/error.go
@@ -11,16 +11,29 @@ const (
 	ErrorCodeLimitReached           = "limit_reached" // Rate limit reached
 	ErrorCodeUnknownError           = "unknown_error" // Unknown error
 	ErrorCodeNotFound               = "not_found"     // Resource not found
+	ErrorCodeInvalidInput           = "invalid_input" // Validation error
 )
 
 // Error is an error returned from the API.
 type Error struct {
 	Code    ErrorCode
 	Message string
+	Details interface{}
 }
 
 func (e Error) Error() string {
 	return fmt.Sprintf("%s (%s)", e.Message, e.Code)
+}
+
+// ErrorInvalidInputDetails contains the details of an 'invalid_input' error.
+type ErrorInvalidInputDetails struct {
+	Fields []*ErrorInvalidInputDetailsField `json:"fields"`
+}
+
+// ErrorInvalidInputDetailsField contains the validation errors reported on a field.
+type ErrorInvalidInputDetailsField struct {
+	Name     string   `json:"name"`
+	Messages []string `json:"messages"`
 }
 
 // IsError returns whether err is an API error with the given error code.
@@ -28,3 +41,6 @@ func IsError(err error, code ErrorCode) bool {
 	apiErr, ok := err.(Error)
 	return ok && apiErr.Code == code
 }
+
+// e := err.(hcloud.Error)
+// details := err.details.(hcloud.InvalidInputDetails)
