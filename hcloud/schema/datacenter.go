bd0aed7503987cc23c84d948aaed121c78f25d86
Nico Schieder <Nico.schieder@gmail.com>
2018-01-10 10:15:35 +0100

    Implement datacenter client (#24)

--- hcloud/schema/datacenter.go
+++ hcloud/schema/datacenter.go
@@ -6,4 +6,18 @@ type Datacenter struct {
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	Location    Location `json:"location"`
+	ServerTypes struct {
+		Supported []int `json:"supported"`
+		Available []int `json:"available"`
+	} `json:"server_types"`
+}
+
+// DatacenterGetResponse defines the schema of the response when retrieving a single datacenter.
+type DatacenterGetResponse struct {
+	Datacenter Datacenter `json:"datacenter"`
+}
+
+// DatacenterListResponse defines the schema of the response when listing datacenters.
+type DatacenterListResponse struct {
+	Datacenters []Datacenter `json:"datacenters"`
 }
--- hcloud/schema_test.go
+++ hcloud/schema_test.go
@@ -251,6 +251,12 @@ func TestDatacenterFromSchema(t *testing.T) {
 	if datacenter.Location == nil || datacenter.Location.ID != 1 {
 		t.Errorf("unexpected Location: %v", datacenter.Location)
 	}
+	if len(datacenter.ServerTypes.Available) != 4 {
+		t.Errorf("unexpected ServerTypes.Available (should be 4): %v", len(datacenter.ServerTypes.Available))
+	}
+	if len(datacenter.ServerTypes.Supported) != 4 {
+		t.Errorf("unexpected ServerTypes.Supported length (should be 4): %v", len(datacenter.ServerTypes.Supported))
+	}
 }
 
 func TestServerFromSchema(t *testing.T) {
