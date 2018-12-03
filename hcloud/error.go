f60481f117595e9232ec4b53222ca634159cdde2
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-12-03 14:30:28 +0100

    Add all available error codes as constant

--- hcloud/error.go
+++ hcloud/error.go
@@ -7,11 +7,19 @@ type ErrorCode string
 
 // Error codes returned from the API.
 const (
-	ErrorCodeServiceError      ErrorCode = "service_error"       // Generic server error
-	ErrorCodeRateLimitExceeded ErrorCode = "rate_limit_exceeded" // Rate limit exceeded
-	ErrorCodeUnknownError      ErrorCode = "unknown_error"       // Unknown error
-	ErrorCodeNotFound          ErrorCode = "not_found"           // Resource not found
-	ErrorCodeInvalidInput      ErrorCode = "invalid_input"       // Validation error
+	ErrorCodeServiceError      ErrorCode = "service_error"           // Generic service error
+	ErrorCodeRateLimitExceeded ErrorCode = "rate_limit_exceeded"     // Rate limit exceeded
+	ErrorCodeUnknownError      ErrorCode = "unknown_error"           // Unknown error
+	ErrorCodeNotFound          ErrorCode = "not_found"               // Resource not found
+	ErrorCodeInvalidInput      ErrorCode = "invalid_input"           // Validation error
+	ErrorCodeForbidden         ErrorCode = "forbidden"               // Insufficient permissions
+	ErrorJSONError             ErrorCode = "json_error"              // Invalid JSON in request
+	ErrorLocked                ErrorCode = "locked"                  // Item is locked (Another action is running)
+	ErrorResourceLimitExceeded ErrorCode = "resource_limit_exceeded" // Resource limit exceeded
+	ErrorResourceUnavailable   ErrorCode = "resource_unavailable"    // Resource currently unavailable
+	ErrorUniquenessError       ErrorCode = "uniqueness_error"        // One or more fields must be unique
+	ErrorProtected             ErrorCode = "protected"               // The actions you are trying is protected
+	ErrorMaintenance           ErrorCode = "maintenance"             // Cannot perform operation due to maintenance
 
 	// Deprecated error codes
 
