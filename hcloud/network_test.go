01f2c62d195253df21b5c89312ae9d24c739d5d0
Marcel Scheck <marcel.scheck@hetzner-cloud.de>
2020-04-28 17:23:11 +0200

    add subnet type cloud

--- hcloud/network_test.go
+++ hcloud/network_test.go
@@ -372,7 +372,7 @@ func TestNetworkClientAddSubnet(t *testing.T) {
 			if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 				t.Fatal(err)
 			}
-			if reqBody.Type != "server" {
+			if reqBody.Type != "cloud" {
 				t.Errorf("unexpected Type: %v", reqBody.Type)
 			}
 			if reqBody.IPRange != "10.0.1.0/24" {
@@ -415,7 +415,7 @@ func TestNetworkClientAddSubnet(t *testing.T) {
 			if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 				t.Fatal(err)
 			}
-			if reqBody.Type != "server" {
+			if reqBody.Type != "cloud" {
 				t.Errorf("unexpected Type: %v", reqBody.Type)
 			}
 			if reqBody.IPRange != "" {
