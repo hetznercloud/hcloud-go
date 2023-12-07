e0d491cff3d1a9badf6adffb3681ae4f93102669
pauhull <pauhull3@gmail.com>
2023-12-07 14:44:21 +0100

    fix generate_schema.sh on BSD based OSes

--- script/generate_schema.sh
+++ script/generate_schema.sh
@@ -2,20 +2,25 @@
 
 set -u
 
-sed="sed -i \'\'"
-if sed --version >/dev/null 2>&1; then # BSD sed exits with 1 on `sed --version`, GNU sed doesn't
-  sed="sed -i" # GNU sed
-fi
+replace() {
+  if sed --version >/dev/null 2>&1; then
+    # GNU
+    sed -i -e "$1" schema.go
+  else
+    # BSD ('' needed or a backup file will be created)
+    sed -i '' -e "$1" schema.go
+  fi
+}
 
 # Delete zz_schema.go if it exists so that there are no compiler errors during generation.
 rm -f zz_schema.go
 
 # Replace "var c converter = &converterImpl{}" with "var c converter" to regenerate the schema.
 # This is done so that there is no compiler error during generation.
-$sed -e "s/var c converter = \&converterImpl{}/var c converter/g" schema.go
+replace "s/var c converter = \&converterImpl{}/var c converter/g"
 
 # Generate zz_schema.go
 go run github.com/jmattheis/goverter/cmd/goverter gen ./...
 
 # After the file is generated we can add the reference back in.
-$sed -e "s/var c converter/var c converter = \&converterImpl{}/g" schema.go
+replace "s/var c converter/var c converter = \&converterImpl{}/g"
