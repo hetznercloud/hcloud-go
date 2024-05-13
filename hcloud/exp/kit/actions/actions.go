d9ace4c21e9df9259d198ef04427b236379b3ac4
jo <ljonas@riseup.net>
2024-05-13 14:15:46 +0200

    add function docs

--- hcloud/exp/kit/actions/actions.go
+++ hcloud/exp/kit/actions/actions.go
@@ -2,6 +2,7 @@ package actions
 
 import "github.com/hetznercloud/hcloud-go/v2/hcloud"
 
+// AppendNextActions return the action and the next actions in a new slice.
 func AppendNextActions(action *hcloud.Action, nextActions []*hcloud.Action) []*hcloud.Action {
 	all := make([]*hcloud.Action, 0, 1+len(nextActions))
 	all = append(all, action)
