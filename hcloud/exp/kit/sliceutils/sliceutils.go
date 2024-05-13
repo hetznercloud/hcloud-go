758b61de54f0bbe79b96cb30e2f61e0ffb6c3226
jo <ljonas@riseup.net>
2024-05-13 10:58:52 +0200

    feat(exp): add sliceutils package

---
+++ hcloud/exp/kit/sliceutils/sliceutils.go
@@ -0,0 +1,12 @@
+package sliceutils
+
+// Map manipulates a slice and transforms it to a slice of another type.
+func Map[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
+	result := make([]R, len(collection))
+
+	for i, item := range collection {
+		result[i] = iteratee(item, i)
+	}
+
+	return result
+}
