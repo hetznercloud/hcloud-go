f627a6800c03867cabafe9f22646a6cef326b272
jo <ljonas@riseup.net>
2024-05-13 15:16:16 +0200

    rename mocks

--- hcloud/exp/mock/client.go
+++ hcloud/exp/mock/client.go
@@ -1,52 +1,53 @@
 package mock
 
 import (
-	hcloud "github.com/hetznercloud/hcloud-go/v2/hcloud"
 	gomock "go.uber.org/mock/gomock"
+
+	hcloud "github.com/hetznercloud/hcloud-go/v2/hcloud"
 )
 
-type MockClient struct {
-	Action           *MockActionClient
-	Certificate      *MockCertificateClient
-	Datacenter       *MockDatacenterClient
-	Firewall         *MockFirewallClient
-	FloatingIP       *MockFloatingIPClient
-	Image            *MockImageClient
-	ISO              *MockISOClient
-	LoadBalancer     *MockLoadBalancerClient
-	LoadBalancerType *MockLoadBalancerTypeClient
-	Location         *MockLocationClient
-	Network          *MockNetworkClient
-	PlacementGroup   *MockPlacementGroupClient
-	PrimaryIP        *MockPrimaryIPClient
-	RDNS             *MockRDNSClient
-	Server           *MockServerClient
-	ServerType       *MockServerTypeClient
-	SSHKey           *MockSSHKeyClient
-	Volume           *MockVolumeClient
+type Client struct {
+	Action           *ActionClient
+	Certificate      *CertificateClient
+	Datacenter       *DatacenterClient
+	Firewall         *FirewallClient
+	FloatingIP       *FloatingIPClient
+	Image            *ImageClient
+	ISO              *ISOClient
+	LoadBalancer     *LoadBalancerClient
+	LoadBalancerType *LoadBalancerTypeClient
+	Location         *LocationClient
+	Network          *NetworkClient
+	PlacementGroup   *PlacementGroupClient
+	PrimaryIP        *PrimaryIPClient
+	RDNS             *RDNSClient
+	Server           *ServerClient
+	ServerType       *ServerTypeClient
+	SSHKey           *SSHKeyClient
+	Volume           *VolumeClient
 }
 
-func NewMockClient(ctrl *gomock.Controller) *MockClient {
-	return &MockClient{
-		Action:           NewMockActionClient(ctrl),
-		Certificate:      NewMockCertificateClient(ctrl),
-		Datacenter:       NewMockDatacenterClient(ctrl),
-		Firewall:         NewMockFirewallClient(ctrl),
-		FloatingIP:       NewMockFloatingIPClient(ctrl),
-		Image:            NewMockImageClient(ctrl),
-		ISO:              NewMockISOClient(ctrl),
-		LoadBalancer:     NewMockLoadBalancerClient(ctrl),
-		LoadBalancerType: NewMockLoadBalancerTypeClient(ctrl),
-		Location:         NewMockLocationClient(ctrl),
-		Network:          NewMockNetworkClient(ctrl),
-		PlacementGroup:   NewMockPlacementGroupClient(ctrl),
-		PrimaryIP:        NewMockPrimaryIPClient(ctrl),
-		RDNS:             NewMockRDNSClient(ctrl),
-		Server:           NewMockServerClient(ctrl),
-		ServerType:       NewMockServerTypeClient(ctrl),
-		SSHKey:           NewMockSSHKeyClient(ctrl),
-		Volume:           NewMockVolumeClient(ctrl),
+func NewMockClient(ctrl *gomock.Controller) *Client {
+	return &Client{
+		Action:           NewActionClient(ctrl),
+		Certificate:      NewCertificateClient(ctrl),
+		Datacenter:       NewDatacenterClient(ctrl),
+		Firewall:         NewFirewallClient(ctrl),
+		FloatingIP:       NewFloatingIPClient(ctrl),
+		Image:            NewImageClient(ctrl),
+		ISO:              NewISOClient(ctrl),
+		LoadBalancer:     NewLoadBalancerClient(ctrl),
+		LoadBalancerType: NewLoadBalancerTypeClient(ctrl),
+		Location:         NewLocationClient(ctrl),
+		Network:          NewNetworkClient(ctrl),
+		PlacementGroup:   NewPlacementGroupClient(ctrl),
+		PrimaryIP:        NewPrimaryIPClient(ctrl),
+		RDNS:             NewRDNSClient(ctrl),
+		Server:           NewServerClient(ctrl),
+		ServerType:       NewServerTypeClient(ctrl),
+		SSHKey:           NewSSHKeyClient(ctrl),
+		Volume:           NewVolumeClient(ctrl),
 	}
 }
 
-func (*MockClient) WithOpts(...hcloud.ClientOption) {}
+func (*Client) WithOpts(...hcloud.ClientOption) {}
