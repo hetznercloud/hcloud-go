b07d7adf0bb08dd372f1f6ff630d16af8cb1265c
Jonas L <jooola@users.noreply.github.com>
2024-05-24 12:03:55 +0200

    feat(exp): add `AppendNextActions` function (#440)
    This function is used to merge both a single action and the next actions
    returned by the API.

---
+++ hcloud/exp/actionutils/actions.go
@@ -0,0 +1,11 @@
+package actionutils
+
+import "github.com/hetznercloud/hcloud-go/v2/hcloud"
+
+// AppendNextActions return the action and the next actions in a new slice.
+func AppendNextActions(action *hcloud.Action, nextActions []*hcloud.Action) []*hcloud.Action {
+	all := make([]*hcloud.Action, 0, 1+len(nextActions))
+	all = append(all, action)
+	all = append(all, nextActions...)
+	return all
+}
---
+++ hcloud/exp/actionutils/actions_test.go
@@ -0,0 +1,18 @@
+package actionutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
+
+func TestAppendNextActions(t *testing.T) {
+	action := &hcloud.Action{ID: 1}
+	nextActions := []*hcloud.Action{{ID: 2}, {ID: 3}}
+
+	actions := AppendNextActions(action, nextActions)
+
+	assert.Equal(t, []*hcloud.Action{{ID: 1}, {ID: 2}, {ID: 3}}, actions)
+}
