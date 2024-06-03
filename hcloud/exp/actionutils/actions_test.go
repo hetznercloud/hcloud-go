cc98ec5eb581bf10d69ccf4b5d6b4c0864e953e6
jo <ljonas@riseup.net>
2024-06-03 09:16:54 +0200

    feat(exp): rename AppendNextActions to AppendNext

--- hcloud/exp/actionutils/actions_test.go
+++ hcloud/exp/actionutils/actions_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/hetznercloud/hcloud-go/v2/hcloud"
 )
 
-func TestAppendNextActions(t *testing.T) {
+func TestAppendNext(t *testing.T) {
 	action := &hcloud.Action{ID: 1}
 	nextActions := []*hcloud.Action{{ID: 2}, {ID: 3}}
 
-	actions := AppendNextActions(action, nextActions)
+	actions := AppendNext(action, nextActions)
 
 	assert.Equal(t, []*hcloud.Action{{ID: 1}, {ID: 2}, {ID: 3}}, actions)
 }
