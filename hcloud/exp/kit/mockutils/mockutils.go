b1cb5df5ddb36e91f15fcd5a9206966dff14b683
jo <ljonas@riseup.net>
2024-06-18 12:00:14 +0200

    feat(exp): add mock utils package

---
+++ hcloud/exp/kit/mockutils/mockutils.go
@@ -0,0 +1,72 @@
+package mockutils
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Request describes a http request that a [httptest.Server] should receive, and the
+// corresponding response to return.
+//
+// Additional checks on the request (e.g. request body) may be added using the
+// [Request.Want] function.
+//
+// The response body is populated from either a JSON struct, or a JSON string.
+type Request struct {
+	Method string
+	Path   string
+	Want   func(t *testing.T, r *http.Request)
+
+	Status  int
+	JSON    any
+	JSONRaw string
+}
+
+// Handler is used with a [httptest.Server] to mock http requests provided by the user.
+//
+// Request matching is based on the request count, and the user provided request will be
+// iterated over.
+func Handler(t *testing.T, requests []Request) http.HandlerFunc {
+	t.Helper()
+
+	index := 0
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if testing.Verbose() {
+			t.Logf("call %d: %s %s\n", index, r.Method, r.RequestURI)
+		}
+
+		if index >= len(requests) {
+			t.Fatalf("received unknown call %d", index)
+		}
+
+		expected := requests[index]
+		require.Equal(t,
+			expected.Method+" "+expected.Path,
+			r.Method+" "+r.RequestURI,
+		)
+
+		if expected.Want != nil {
+			expected.Want(t, r)
+		}
+
+		w.WriteHeader(expected.Status)
+		switch {
+		case expected.JSON != nil:
+			w.Header().Set("Content-Type", "application/json")
+			if err := json.NewEncoder(w).Encode(expected.JSON); err != nil {
+				t.Fatal(err)
+			}
+		case expected.JSONRaw != "":
+			w.Header().Set("Content-Type", "application/json")
+			_, err := w.Write([]byte(expected.JSONRaw))
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		index++
+	})
+}
