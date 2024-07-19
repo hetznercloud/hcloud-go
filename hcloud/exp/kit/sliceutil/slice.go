431c7ce332e6dfec058e56e3e239603a10a6410b
jo <ljonas@riseup.net>
2024-07-19 11:44:27 +0200

    feat(exp): add sliceutil package

---
+++ hcloud/exp/kit/sliceutil/slice.go
@@ -0,0 +1,8 @@
+package sliceutil
+
+func Batches[T any](all []T, size int) (batches [][]T) {
+	for size < len(all) {
+		all, batches = all[size:], append(batches, all[:size])
+	}
+	return append(batches, all)
+}
