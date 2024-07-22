f4ad6bc93ff5017dda1b71a1606b67a79b56eb57
Jonas L. <jooola@users.noreply.github.com>
2024-07-22 15:32:27 +0200

    feat(exp): add sliceutil package (#489)
    - Add a Batches utility to split slices into batches.

---
+++ hcloud/exp/kit/sliceutil/slice.go
@@ -0,0 +1,9 @@
+package sliceutil
+
+// Batches splits a slice into multiple batches of a desired size.
+func Batches[T any](all []T, size int) (batches [][]T) {
+	for size < len(all) {
+		all, batches = all[size:], append(batches, all[:size])
+	}
+	return append(batches, all)
+}
