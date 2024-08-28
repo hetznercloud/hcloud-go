824cb91805e3af2357e84ba0315981a72f845307
jo <ljonas@riseup.net>
2024-08-28 09:57:43 +0200

    feat(exp): add actionutil.AllForResource
    Allow the users to fetch actions related to a specific resource.

--- hcloud/exp/actionutil/actions.go
+++ hcloud/exp/actionutil/actions.go
@@ -1,6 +1,11 @@
 package actionutil
 
-import "github.com/hetznercloud/hcloud-go/v2/hcloud"
+import (
+	"context"
+	"slices"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
 
 // AppendNext return the action and the next actions in a new slice.
 func AppendNext(action *hcloud.Action, nextActions []*hcloud.Action) []*hcloud.Action {
@@ -8,4 +13,25 @@ func AppendNext(action *hcloud.Action, nextActions []*hcloud.Action) []*hcloud.A
 	all = append(all, action)
 	all = append(all, nextActions...)
 	return all
+}
+
+func AllForResource(
+	ctx context.Context,
+	actionClient *hcloud.ResourceActionClient,
+	opts hcloud.ActionListOpts,
+	resourceType hcloud.ActionResourceType,
+	resourceID int64,
+) ([]*hcloud.Action, error) {
+	actions, err := actionClient.All(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	actions = slices.Clip(slices.DeleteFunc(actions, func(action *hcloud.Action) bool {
+		return !slices.ContainsFunc(action.Resources, func(resource *hcloud.ActionResource) bool {
+			return resource.Type == resourceType && resource.ID == resourceID
+		})
+	}))
+
+	return actions, nil
 }
