hetznercloud/hcloud-go

commit 62ecd3e83c03556fdf011dc4d2d916d7146f1b4f

feat: redefine `api_endpoint` metric labels (#626)
- Keep path variables hints in the api_endpoint label
- Do not compute the api_endpoint label from the URL path
- Pass down the api_endpoint label using a context value

hcloud/exp/ctxutil/ctxutil.go
@@ -1,0 +1,25 @@
+package ctxutil
+
+import (
+	"context"
+	"strings"
+)
+
+// key is an unexported type to prevents collisions with keys defined in other packages.
+type key struct{}
+
+// opPathKey is the key for operation path in Contexts.
+var opPathKey = key{}
+
+func SetOpPath(ctx context.Context, path string) context.Context {
+	path, _, _ = strings.Cut(path, "?")
+	path = strings.ReplaceAll(path, "%d", "-")
+	path = strings.ReplaceAll(path, "%s", "-")
+
+	return context.WithValue(ctx, opPathKey, path)
+}
+
+func OpPath(ctx context.Context) string {
+	result := ctx.Value(opPathKey).(string)
+	return result
+}

hcloud/internal/instrumentation/metrics.go
@@ -3,12 +3,13 @@ package instrumentation
 import (
 	"fmt"
 	"net/http"
-	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 )
 
 type Instrumenter struct {
@@ -73,8 +74,11 @@ func (i *Instrumenter) instrumentRoundTripperEndpoint(counter *prometheus.Counte
 	return func(r *http.Request) (*http.Response, error) {
 		resp, err := next.RoundTrip(r)
 		if err == nil {
-			statusCode := strconv.Itoa(resp.StatusCode)
-			counter.WithLabelValues(statusCode, strings.ToLower(resp.Request.Method), preparePathForLabel(resp.Request.URL.Path)).Inc()
+			counter.WithLabelValues(
+				strconv.Itoa(resp.StatusCode),
+				strings.ToLower(resp.Request.Method),
+				ctxutil.OpPath(r.Context()),
+			).Inc()
 		}
 
 		return resp, err
@@ -100,19 +104,4 @@ func registerOrReuse[C prometheus.Collector](registry prometheus.Registerer, col
 	}
 
 	return collector
-}
-
-var pathLabelRegexp = regexp.MustCompile("[^a-z/_]+")
-
-func preparePathForLabel(path string) string {
-	path = strings.ToLower(path)
-
-	// replace all numbers and chars that are not a-z, / or _
-	path = pathLabelRegexp.ReplaceAllString(path, "")
-
-	// replace all artifacts of number replacement (//)
-	path = strings.ReplaceAll(path, "//", "/")
-
-	// replace the /v/ that indicated the API version
-	return strings.Replace(path, "/v/", "/", 1)
 }

hcloud/internal/instrumentation/metrics_test.go
@@ -6,32 +6,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func Test_preparePath(t *testing.T) {
-	tests := []struct {
-		name string
-		path string
-		want string
-	}{
-		{
-			"simple test",
-			"/v1/volumes/123456",
-			"/volumes/",
-		},
-		{
-			"simple test",
-			"/v1/volumes/123456/actions/attach",
-			"/volumes/actions/attach",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := preparePathForLabel(tt.path); got != tt.want {
-				t.Errorf("preparePathForLabel() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestMultipleInstrumentedClients(t *testing.T) {
 	reg := prometheus.NewRegistry()
 

hcloud/action.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
 )
 
@@ -153,7 +154,10 @@ func (c *ResourceActionClient) getBaseURL() string {
 
 // GetByID retrieves an action by its ID. If the action does not exist, nil is returned.
 func (c *ResourceActionClient) GetByID(ctx context.Context, id int64) (*Action, *Response, error) {
-	reqPath := fmt.Sprintf("%s/actions/%d", c.getBaseURL(), id)
+	opPath := c.getBaseURL() + "/actions/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, id)
 
 	respBody, resp, err := getRequest[schema.ActionGetResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -170,7 +174,10 @@ func (c *ResourceActionClient) GetByID(ctx context.Context, id int64) (*Action, 
 // Please note that filters specified in opts are not taken into account
 // when their value corresponds to their zero value or when they are empty.
 func (c *ResourceActionClient) List(ctx context.Context, opts ActionListOpts) ([]*Action, *Response, error) {
-	reqPath := fmt.Sprintf("%s/actions?%s", c.getBaseURL(), opts.values().Encode())
+	opPath := c.getBaseURL() + "/actions?%s"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, opts.values().Encode())
 
 	respBody, resp, err := getRequest[schema.ActionListResponse](ctx, c.client, reqPath)
 	if err != nil {

hcloud/certificate.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
 )
 
@@ -95,7 +96,10 @@ type CertificateClient struct {
 
 // GetByID retrieves a Certificate by its ID. If the Certificate does not exist, nil is returned.
 func (c *CertificateClient) GetByID(ctx context.Context, id int64) (*Certificate, *Response, error) {
-	reqPath := fmt.Sprintf("/certificates/%d", id)
+	const opPath = "/certificates/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, id)
 
 	respBody, resp, err := getRequest[schema.CertificateGetResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -143,7 +147,10 @@ func (l CertificateListOpts) values() url.Values {
 // Please note that filters specified in opts are not taken into account
 // when their value corresponds to their zero value or when they are empty.
 func (c *CertificateClient) List(ctx context.Context, opts CertificateListOpts) ([]*Certificate, *Response, error) {
-	reqPath := fmt.Sprintf("/certificates?%s", opts.values().Encode())
+	const opPath = "/certificates?%s"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, opts.values().Encode())
 
 	respBody, resp, err := getRequest[schema.CertificateListResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -227,6 +234,11 @@ func (c *CertificateClient) Create(ctx context.Context, opts CertificateCreateOp
 func (c *CertificateClient) CreateCertificate(
 	ctx context.Context, opts CertificateCreateOpts,
 ) (CertificateCreateResult, *Response, error) {
+	const opPath = "/certificates"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := opPath
+
 	result := CertificateCreateResult{}
 
 	if err := opts.Validate(); err != nil {
@@ -253,8 +265,6 @@ func (c *CertificateClient) CreateCertificate(
 		reqBody.Labels = &opts.Labels
 	}
 
-	reqPath := "/certificates"
-
 	respBody, resp, err := postRequest[schema.CertificateCreateResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
 		return result, resp, err
@@ -276,6 +286,11 @@ type CertificateUpdateOpts struct {
 
 // Update updates a Certificate.
 func (c *CertificateClient) Update(ctx context.Context, certificate *Certificate, opts CertificateUpdateOpts) (*Certificate, *Response, error) {
+	const opPath = "/certificates/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, certificate.ID)
+
 	reqBody := schema.CertificateUpdateRequest{}
 	if opts.Name != "" {
 		reqBody.Name = &opts.Name
@@ -283,8 +298,6 @@ func (c *CertificateClient) Update(ctx context.Context, certificate *Certificate
 	if opts.Labels != nil {
 		reqBody.Labels = &opts.Labels
 	}
-
-	reqPath := fmt.Sprintf("/certificates/%d", certificate.ID)
 
 	respBody, resp, err := putRequest[schema.CertificateUpdateResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
@@ -296,14 +309,20 @@ func (c *CertificateClient) Update(ctx context.Context, certificate *Certificate
 
 // Delete deletes a certificate.
 func (c *CertificateClient) Delete(ctx context.Context, certificate *Certificate) (*Response, error) {
-	reqPath := fmt.Sprintf("/certificates/%d", certificate.ID)
+	const opPath = "/certificates/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, certificate.ID)
 
 	return deleteRequestNoResult(ctx, c.client, reqPath)
 }
 
 // RetryIssuance retries the issuance of a failed managed certificate.
 func (c *CertificateClient) RetryIssuance(ctx context.Context, certificate *Certificate) (*Action, *Response, error) {
-	reqPath := fmt.Sprintf("/certificates/%d/actions/retry", certificate.ID)
+	const opPath = "/certificates/%d/actions/retry"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, certificate.ID)
 
 	respBody, resp, err := postRequest[schema.CertificateIssuanceRetryResponse](ctx, c.client, reqPath, nil)
 	if err != nil {

hcloud/datacenter.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"strconv"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
 )
 
@@ -32,7 +33,10 @@ type DatacenterClient struct {
 
 // GetByID retrieves a datacenter by its ID. If the datacenter does not exist, nil is returned.
 func (c *DatacenterClient) GetByID(ctx context.Context, id int64) (*Datacenter, *Response, error) {
-	reqPath := fmt.Sprintf("/datacenters/%d", id)
+	const opPath = "/datacenters/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, id)
 
 	respBody, resp, err := getRequest[schema.DatacenterGetResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -84,7 +88,10 @@ func (l DatacenterListOpts) values() url.Values {
 // Please note that filters specified in opts are not taken into account
 // when their value corresponds to their zero value or when they are empty.
 func (c *DatacenterClient) List(ctx context.Context, opts DatacenterListOpts) ([]*Datacenter, *Response, error) {
-	reqPath := fmt.Sprintf("/datacenters?%s", opts.values().Encode())
+	const opPath = "/datacenters?%s"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, opts.values().Encode())
 
 	respBody, resp, err := getRequest[schema.DatacenterListResponse](ctx, c.client, reqPath)
 	if err != nil {

hcloud/exp/ctxutil/ctxutil_test.go
@@ -1,0 +1,34 @@
+package ctxutil
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpPath(t *testing.T) {
+	for _, tt := range []struct {
+		path string
+		want string
+	}{
+		{
+			path: "/resource/%s/nested/%s/%d",
+			want: "/resource/-/nested/-/-",
+		},
+		{
+			path: "/certificates/%d",
+			want: "/certificates/-",
+		},
+		{
+			path: "/servers/%d/metrics?%s",
+			want: "/servers/-/metrics",
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			ctx := context.Background()
+
+			require.Equal(t, tt.want, OpPath(SetOpPath(ctx, tt.path)))
+		})
+	}
+}

hcloud/firewall.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
 )
 
@@ -93,7 +94,10 @@ type FirewallClient struct {
 
 // GetByID retrieves a Firewall by its ID. If the Firewall does not exist, nil is returned.
 func (c *FirewallClient) GetByID(ctx context.Context, id int64) (*Firewall, *Response, error) {
-	reqPath := fmt.Sprintf("/firewalls/%d", id)
+	const opPath = "/firewalls/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, id)
 
 	respBody, resp, err := getRequest[schema.FirewallGetResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -142,7 +146,10 @@ func (l FirewallListOpts) values() url.Values {
 // Please note that filters specified in opts are not taken into account
 // when their value corresponds to their zero value or when they are empty.
 func (c *FirewallClient) List(ctx context.Context, opts FirewallListOpts) ([]*Firewall, *Response, error) {
-	reqPath := fmt.Sprintf("/firewalls?%s", opts.values().Encode())
+	const opPath = "/firewalls?%s"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, opts.values().Encode())
 
 	respBody, resp, err := getRequest[schema.FirewallListResponse](ctx, c.client, reqPath)
 	if err != nil {
@@ -189,15 +196,18 @@ type FirewallCreateResult struct {
 
 // Create creates a new Firewall.
 func (c *FirewallClient) Create(ctx context.Context, opts FirewallCreateOpts) (FirewallCreateResult, *Response, error) {
+	const opPath = "/firewalls"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
 	result := FirewallCreateResult{}
+
+	reqPath := opPath
 
 	if err := opts.Validate(); err != nil {
 		return result, nil, err
 	}
 
 	reqBody := firewallCreateOptsToSchema(opts)
-
-	reqPath := "/firewalls"
 
 	respBody, resp, err := postRequest[schema.FirewallCreateResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
@@ -218,6 +228,11 @@ type FirewallUpdateOpts struct {
 
 // Update updates a Firewall.
 func (c *FirewallClient) Update(ctx context.Context, firewall *Firewall, opts FirewallUpdateOpts) (*Firewall, *Response, error) {
+	const opPath = "/firewalls/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, firewall.ID)
+
 	reqBody := schema.FirewallUpdateRequest{}
 	if opts.Name != "" {
 		reqBody.Name = &opts.Name
@@ -225,8 +240,6 @@ func (c *FirewallClient) Update(ctx context.Context, firewall *Firewall, opts Fi
 	if opts.Labels != nil {
 		reqBody.Labels = &opts.Labels
 	}
-
-	reqPath := fmt.Sprintf("/firewalls/%d", firewall.ID)
 
 	respBody, resp, err := putRequest[schema.FirewallUpdateResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
@@ -238,7 +251,10 @@ func (c *FirewallClient) Update(ctx context.Context, firewall *Firewall, opts Fi
 
 // Delete deletes a Firewall.
 func (c *FirewallClient) Delete(ctx context.Context, firewall *Firewall) (*Response, error) {
-	reqPath := fmt.Sprintf("/firewalls/%d", firewall.ID)
+	const opPath = "/firewalls/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, firewall.ID)
 
 	return deleteRequestNoResult(ctx, c.client, reqPath)
 }
@@ -250,9 +266,12 @@ type FirewallSetRulesOpts struct {
 
 // SetRules sets the rules of a Firewall.
 func (c *FirewallClient) SetRules(ctx context.Context, firewall *Firewall, opts FirewallSetRulesOpts) ([]*Action, *Response, error) {
-	reqBody := firewallSetRulesOptsToSchema(opts)
+	const opPath = "/firewalls/%d/actions/set_rules"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
 
-	reqPath := fmt.Sprintf("/firewalls/%d/actions/set_rules", firewall.ID)
+	reqPath := fmt.Sprintf(opPath, firewall.ID)
+
+	reqBody := firewallSetRulesOptsToSchema(opts)
 
 	respBody, resp, err := postRequest[schema.FirewallActionSetRulesResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
@@ -263,14 +282,17 @@ func (c *FirewallClient) SetRules(ctx context.Context, firewall *Firewall, opts 
 }
 
 func (c *FirewallClient) ApplyResources(ctx context.Context, firewall *Firewall, resources []FirewallResource) ([]*Action, *Response, error) {
+	const opPath = "/firewalls/%d/actions/apply_to_resources"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, firewall.ID)
+
 	applyTo := make([]schema.FirewallResource, len(resources))
 	for i, r := range resources {
 		applyTo[i] = firewallResourceToSchema(r)
 	}
 
 	reqBody := schema.FirewallActionApplyToResourcesRequest{ApplyTo: applyTo}
-
-	reqPath := fmt.Sprintf("/firewalls/%d/actions/apply_to_resources", firewall.ID)
 
 	respBody, resp, err := postRequest[schema.FirewallActionApplyToResourcesResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {
@@ -281,14 +303,17 @@ func (c *FirewallClient) ApplyResources(ctx context.Context, firewall *Firewall,
 }
 
 func (c *FirewallClient) RemoveResources(ctx context.Context, firewall *Firewall, resources []FirewallResource) ([]*Action, *Response, error) {
+	const opPath = "/firewalls/%d/actions/remove_from_resources"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, firewall.ID)
+
 	removeFrom := make([]schema.FirewallResource, len(resources))
 	for i, r := range resources {
 		removeFrom[i] = firewallResourceToSchema(r)
 	}
 
 	reqBody := schema.FirewallActionRemoveFromResourcesRequest{RemoveFrom: removeFrom}
-
-	reqPath := fmt.Sprintf("/firewalls/%d/actions/remove_from_resources", firewall.ID)
 
 	respBody, resp, err := postRequest[schema.FirewallActionRemoveFromResourcesResponse](ctx, c.client, reqPath, reqBody)
 	if err != nil {

commit 624a4460e73e89a0e1ee012677c343c6c0ad790e

fix: panic when a request did not set the OpPath for instrumentation (#635)
When an instrumented client (API or Metadata) made a request that did
not explicitly specify the OpPath through `ctxutil.SetOpPath()` that
instrumentation code caused a panic.

This is now handled by adding back a fallback if the explicit OpPath is
not set. The fallback uses the same patterns to create the OpPath as our
manual paths.

This only happened for the Metadata client and any custom
clients/requests that users added besides the official endpoints
implemented in hcloud-go.

Bug was introduced in #626, thanks to @lukasmetzner for finding and
reporting.

Co-authored-by: Jonas Lammler <jonas.lammler@hetzner-cloud.de>

hcloud/exp/ctxutil/ctxutil.go
@@ -11,6 +11,7 @@ type key struct{}
 // opPathKey is the key for operation path in Contexts.
 var opPathKey = key{}
 
+// SetOpPath processes the operation path and save it in the context before returning it.
 func SetOpPath(ctx context.Context, path string) context.Context {
 	path, _, _ = strings.Cut(path, "?")
 	path = strings.ReplaceAll(path, "%d", "-")
@@ -19,7 +20,11 @@ func SetOpPath(ctx context.Context, path string) context.Context {
 	return context.WithValue(ctx, opPathKey, path)
 }
 
+// OpPath returns the operation path from the context.
 func OpPath(ctx context.Context) string {
-	result := ctx.Value(opPathKey).(string)
+	result, ok := ctx.Value(opPathKey).(string)
+	if !ok {
+		return ""
+	}
 	return result
 }

hcloud/internal/instrumentation/metrics.go
@@ -3,6 +3,7 @@ package instrumentation
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -74,10 +75,16 @@ func (i *Instrumenter) instrumentRoundTripperEndpoint(counter *prometheus.Counte
 	return func(r *http.Request) (*http.Response, error) {
 		resp, err := next.RoundTrip(r)
 		if err == nil {
+			apiEndpoint := ctxutil.OpPath(r.Context())
+			// If the request does not set the operation path, we must construct it. Happens e.g. for
+			// user crafted requests.
+			if apiEndpoint == "" {
+				apiEndpoint = preparePathForLabel(resp.Request.URL.Path)
+			}
 			counter.WithLabelValues(
 				strconv.Itoa(resp.StatusCode),
 				strings.ToLower(resp.Request.Method),
-				ctxutil.OpPath(r.Context()),
+				apiEndpoint,
 			).Inc()
 		}
 
@@ -104,4 +111,18 @@ func registerOrReuse[C prometheus.Collector](registry prometheus.Registerer, col
 	}
 
 	return collector
+}
+
+var pathLabelRegexp = regexp.MustCompile("[^a-z/_]+")
+
+func preparePathForLabel(path string) string {
+	path = strings.ToLower(path)
+
+	// replace the /v1/ that indicated the API version
+	path, _ = strings.CutPrefix(path, "/v1")
+
+	// replace all numbers and chars that are not a-z, / or _
+	path = pathLabelRegexp.ReplaceAllString(path, "-")
+
+	return path
 }

hcloud/metadata/client.go
@@ -2,6 +2,7 @@ package metadata
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"net"
@@ -12,6 +13,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/internal/instrumentation"
 )
 
@@ -80,9 +82,14 @@ func NewClient(options ...ClientOption) *Client {
 
 // get executes an HTTP request against the API.
 func (c *Client) get(path string) (string, error) {
-	url := c.endpoint + path
+	ctx := ctxutil.SetOpPath(context.Background(), path)
 
-	resp, err := c.httpClient.Get(url)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.endpoint+path, http.NoBody)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return "", err
 	}

hcloud/client_generic_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/mockutil"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
 )
@@ -23,7 +24,10 @@ func TestGenericRequest(t *testing.T) {
 			},
 		})
 
-		respBody, resp, err := getRequest[schema.ActionGetResponse](ctx, client, "/resource")
+		const opPath = "/resource"
+		ctx = ctxutil.SetOpPath(ctx, opPath)
+
+		respBody, resp, err := getRequest[schema.ActionGetResponse](ctx, client, opPath)
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 		require.NotNil(t, respBody)
@@ -47,7 +51,10 @@ func TestGenericRequest(t *testing.T) {
 			},
 		})
 
-		respBody, resp, err := postRequest[schema.ActionGetResponse](ctx, client, "/resource", map[string]string{"hello": "world"})
+		const opPath = "/resource"
+		ctx = ctxutil.SetOpPath(ctx, opPath)
+
+		respBody, resp, err := postRequest[schema.ActionGetResponse](ctx, client, opPath, map[string]string{"hello": "world"})
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 		require.NotNil(t, respBody)
@@ -71,7 +78,10 @@ func TestGenericRequest(t *testing.T) {
 			},
 		})
 
-		respBody, resp, err := putRequest[schema.ActionGetResponse](ctx, client, "/resource", map[string]string{"hello": "world"})
+		const opPath = "/resource"
+		ctx = ctxutil.SetOpPath(ctx, opPath)
+
+		respBody, resp, err := putRequest[schema.ActionGetResponse](ctx, client, opPath, map[string]string{"hello": "world"})
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 		require.NotNil(t, respBody)
@@ -90,7 +100,10 @@ func TestGenericRequest(t *testing.T) {
 			},
 		})
 
-		respBody, resp, err := deleteRequest[schema.ActionGetResponse](ctx, client, "/resource")
+		const opPath = "/resource"
+		ctx = ctxutil.SetOpPath(ctx, opPath)
+
+		respBody, resp, err := deleteRequest[schema.ActionGetResponse](ctx, client, opPath)
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 		require.NotNil(t, respBody)
@@ -108,7 +121,10 @@ func TestGenericRequest(t *testing.T) {
 			},
 		})
 
-		resp, err := deleteRequestNoResult(ctx, client, "/resource")
+		const opPath = "/resource"
+		ctx = ctxutil.SetOpPath(ctx, opPath)
+
+		resp, err := deleteRequestNoResult(ctx, client, opPath)
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 	})

hcloud/client_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -28,6 +29,8 @@ func makeTestUtils(t *testing.T) (context.Context, *mockutil.Server, *Client) {
 		WithEndpoint(server.URL),
 		WithRetryOpts(RetryOpts{BackoffFunc: ConstantBackoff(0), MaxRetries: 5}),
 		WithPollOpts(PollOpts{BackoffFunc: ConstantBackoff(0)}),
+		// This makes sure that our instrumentation does not cause any panics/errors
+		WithInstrumentation(prometheus.DefaultRegisterer),
 	)
 
 	return ctx, server, client
@@ -63,6 +66,8 @@ func newTestEnvWithServer(server *httptest.Server, mux *http.ServeMux) testEnv {
 		WithPollOpts(PollOpts{
 			BackoffFunc: ConstantBackoff(0),
 		}),
+		// This makes sure that our instrumentation does not cause any panics/errors
+		WithInstrumentation(prometheus.DefaultRegisterer),
 	)
 	return testEnv{
 		Server: server,

hcloud/internal/instrumentation/metrics_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMultipleInstrumentedClients(t *testing.T) {
@@ -14,4 +15,25 @@ func TestMultipleInstrumentedClients(t *testing.T) {
 		New("test", reg).InstrumentedRoundTripper()
 		New("test", reg).InstrumentedRoundTripper()
 	})
+}
+
+func TestPreparePathForLabel(t *testing.T) {
+	tests := []struct {
+		path string
+		want string
+	}{
+		{
+			"/v1/volumes/123456",
+			"/volumes/-",
+		},
+		{
+			"/v1/volumes/123456/actions/attach",
+			"/volumes/-/actions/attach",
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			assert.Equal(t, tt.want, preparePathForLabel(tt.path))
+		})
+	}
 }

hcloud/metadata/client_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -27,6 +28,8 @@ func newTestEnv() testEnv {
 	server := httptest.NewServer(mux)
 	client := NewClient(
 		WithEndpoint(server.URL),
+		// This makes sure that our instrumentation does not cause any panics/errors
+		WithInstrumentation(prometheus.DefaultRegisterer),
 	)
 	return testEnv{
 		Server: server,