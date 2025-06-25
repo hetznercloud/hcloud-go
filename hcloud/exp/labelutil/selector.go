de604612967eafcd5ecef1c5b765ad8af307187a
Jonas L. <jooola@users.noreply.github.com>
2025-06-25 15:42:57 +0200

    docs: update links to api specification (sweep) (#670)

--- hcloud/exp/labelutil/selector.go
+++ hcloud/exp/labelutil/selector.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-// Selector combines the label set into a [label selector](https://docs.hetzner.cloud/#label-selector) that only selects
+// Selector combines the label set into a [label selector](https://docs.hetzner.cloud/reference/cloud#label-selector) that only selects
 // resources have all specified labels set.
 //
 // The selector string can be used to filter resources when listing, for example with [hcloud.ServerClient.AllWithOpts()].
