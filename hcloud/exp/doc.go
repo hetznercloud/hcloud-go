3a9fde812fa607c90e7bad96b0eb366390e5430e
Lukas Metzner <lukasmetzner99@gmail.com>
2025-07-17 10:06:31 +0200

    Update hcloud/exp/doc.go
    Co-authored-by: Jonas L. <jooola@users.noreply.github.com>

--- hcloud/exp/doc.go
+++ hcloud/exp/doc.go
@@ -1,4 +1,4 @@
 // Package exp is a namespace that holds experimental features for the `hcloud-go` library.
 //
-// Experimental: Breaking changes may occur without notice.
+// Experimental: Breaking changes may occur within minor releases.
 package exp
