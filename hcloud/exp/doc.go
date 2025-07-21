ef3309a27fe2a08954de2618a90bf193062dccaf
jo <ljonas@riseup.net>
2025-07-21 10:52:03 +0200

    update exp package docs

--- hcloud/exp/doc.go
+++ hcloud/exp/doc.go
@@ -1,4 +1,4 @@
 // Package exp is a namespace that holds experimental features for the `hcloud-go` library.
 //
-// Experimental: Breaking changes may occur within minor releases.
+// Experimental: `exp` package is experimental, breaking changes may occur within minor releases.
 package exp
