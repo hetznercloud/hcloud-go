caf2f2aa5ed2ec85fc0553bc2343d8dd2c5cd0a4
Lukas Metzner <lukasmetzner99@gmail.com>
2025-08-20 14:01:32 +0200

    feat: Storage Box CRUD operations (#694)
    Adds the new Storage Box client with the necessary types and CRUD
    operations.
    
    API Reference:
    https://docs.hetzner.cloud/reference/hetzner#storage-boxes
    
    ---------
    
    Co-authored-by: Julian Tölle <julian.toelle@hetzner-cloud.de>

---
+++ hcloud/schema/storage_box.go
@@ -0,0 +1,105 @@
+package schema
+
+import "time"
+
+// StorageBox defines the schema of a storage box.
+type StorageBox struct {
+	ID             int64                    `json:"id"`
+	Username       *string                  `json:"username,omitempty"`
+	Status         string                   `json:"status"`
+	Name           string                   `json:"name"`
+	StorageBoxType StorageBoxType           `json:"storage_box_type"`
+	Location       Location                 `json:"location"`
+	AccessSettings StorageBoxAccessSettings `json:"access_settings"`
+	Server         *string                  `json:"server"`
+	System         *string                  `json:"system"`
+	Stats          *StorageBoxStats         `json:"stats"`
+	Labels         map[string]string        `json:"labels"`
+	Protection     StorageBoxProtection     `json:"protection"`
+	SnapshotPlan   *StorageBoxSnapshotPlan  `json:"snapshot_plan"`
+	Created        time.Time                `json:"created"`
+}
+
+// StorageBoxAccessSettings defines the schema of a storage box's access settings.
+type StorageBoxAccessSettings struct {
+	ReachableExternally bool `json:"reachable_externally"`
+	SambaEnabled        bool `json:"samba_enabled"`
+	SSHEnabled          bool `json:"ssh_enabled"`
+	WebDAVEnabled       bool `json:"webdav_enabled"`
+	ZFSEnabled          bool `json:"zfs_enabled"`
+}
+
+// StorageBoxStats defines the schema of a storage box's disk usage statistics.
+type StorageBoxStats struct {
+	Size          uint64 `json:"size"`
+	SizeData      uint64 `json:"size_data"`
+	SizeSnapshots uint64 `json:"size_snapshots"`
+}
+
+// StorageBoxProtection defines the schema of a storage box's resource protection.
+type StorageBoxProtection struct {
+	Delete bool `json:"delete"`
+}
+
+// StorageBoxSnapshotPlan defines the schema of a storage box's snapshot plan.
+type StorageBoxSnapshotPlan struct {
+	MaxSnapshots int  `json:"max_snapshots"`
+	Minute       *int `json:"minute,omitempty"`
+	Hour         *int `json:"hour,omitempty"`
+	DayOfWeek    *int `json:"day_of_week,omitempty"`
+	DayOfMonth   *int `json:"day_of_month,omitempty"`
+}
+
+// StorageBoxGetResponse defines the schema of the response when
+// retrieving a single storage box.
+type StorageBoxGetResponse struct {
+	StorageBox StorageBox `json:"storage_box"`
+}
+
+// StorageBoxListResponse defines the schema of the response when
+// listing storage boxes.
+type StorageBoxListResponse struct {
+	StorageBoxes []StorageBox `json:"storage_boxes"`
+}
+
+// StorageBoxCreateRequest defines the schema for the request to
+// create a storage box.
+type StorageBoxCreateRequest struct {
+	Name           string                                `json:"name"`
+	StorageBoxType IDOrName                              `json:"storage_box_type"`
+	Location       string                                `json:"location"`
+	Labels         *map[string]string                    `json:"labels,omitempty"`
+	Password       string                                `json:"password"`
+	SSHKeys        []string                              `json:"ssh_keys,omitempty"`
+	AccessSettings StorageBoxCreateRequestAccessSettings `json:"access_settings,omitempty"`
+}
+
+type StorageBoxCreateRequestAccessSettings struct {
+	ReachableExternally *bool `json:"reachable_externally,omitempty"`
+	SambaEnabled        *bool `json:"samba_enabled,omitempty"`
+	SSHEnabled          *bool `json:"ssh_enabled,omitempty"`
+	WebDAVEnabled       *bool `json:"webdav_enabled,omitempty"`
+	ZFSEnabled          *bool `json:"zfs_enabled,omitempty"`
+}
+
+// StorageBoxCreateResponse defines the schema of the response when
+// creating a storage box.
+type StorageBoxCreateResponse struct {
+	StorageBox StorageBox `json:"storage_box"`
+	Action     Action     `json:"action"`
+}
+
+// StorageBoxUpdateRequest defines the schema of the request to update a storage box.
+type StorageBoxUpdateRequest struct {
+	Name   string            `json:"name,omitempty"`
+	Labels map[string]string `json:"labels,omitempty"`
+}
+
+// StorageBoxUpdateResponse defines the schema of the response when updating a storage box.
+type StorageBoxUpdateResponse struct {
+	StorageBox StorageBox `json:"storage_box"`
+}
+
+type StorageBoxFoldersResponse struct {
+	Folders []string `json:"folders"`
+}
---
+++ hcloud/storage_box.go
@@ -0,0 +1,280 @@
+package hcloud
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/exp/ctxutil"
+	"github.com/hetznercloud/hcloud-go/v2/hcloud/schema"
+)
+
+// StorageBox represents a storage box in Hetzner.
+type StorageBox struct {
+	ID             int64
+	Username       *string
+	Status         StorageBoxStatus
+	Name           string
+	StorageBoxType *StorageBoxType
+	Location       *Location
+	AccessSettings StorageBoxAccessSettings
+	Server         *string
+	System         *string
+	Stats          *StorageBoxStats
+	Labels         map[string]string
+	Protection     StorageBoxProtection
+	SnapshotPlan   *StorageBoxSnapshotPlan
+	Created        time.Time
+}
+
+// StorageBoxAccessSettings represents the access settings of a storage box.
+type StorageBoxAccessSettings struct {
+	ReachableExternally bool
+	SambaEnabled        bool
+	SSHEnabled          bool
+	WebDAVEnabled       bool
+	ZFSEnabled          bool
+}
+
+// StorageBoxStats represents the disk usage statistics of a storage box.
+type StorageBoxStats struct {
+	Size          uint64
+	SizeData      uint64
+	SizeSnapshots uint64
+}
+
+// StorageBoxProtection represents the protection level of a storage box.
+type StorageBoxProtection struct {
+	Delete bool
+}
+
+// StorageBoxSnapshotPlan represents the snapshot plan of a storage box.
+type StorageBoxSnapshotPlan struct {
+	MaxSnapshots int
+	Minute       *int
+	Hour         *int
+	DayOfWeek    *int
+	DayOfMonth   *int
+}
+
+// StorageBoxStatus specifies a storage box's status.
+type StorageBoxStatus string
+
+const (
+	// StorageBoxStatusActive is the status when a storage box is active.
+	StorageBoxStatusActive StorageBoxStatus = "active"
+
+	// StorageBoxStatusInitializing is the status when a storage box is initializing.
+	StorageBoxStatusInitializing StorageBoxStatus = "initializing"
+
+	// StorageBoxStatusLocked is the status when a storage box is locked.
+	StorageBoxStatusLocked StorageBoxStatus = "locked"
+)
+
+// StorageBoxClient is a client for the storage box API.
+type StorageBoxClient struct {
+	client *Client
+	Action *ResourceActionClient
+}
+
+// GetByID retrieves a storage box by its ID. If the storage box does not exist, nil is returned.
+func (c *StorageBoxClient) GetByID(ctx context.Context, id int64) (*StorageBox, *Response, error) {
+	const opPath = "/storage_boxes/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, id)
+
+	respBody, resp, err := getRequest[schema.StorageBoxGetResponse](ctx, c.client, reqPath)
+	if err != nil {
+		if IsError(err, ErrorCodeNotFound) {
+			return nil, resp, nil
+		}
+		return nil, resp, err
+	}
+
+	return StorageBoxFromSchema(respBody.StorageBox), resp, nil
+}
+
+// GetByName retrieves a storage box by its name. If the storage box does not exist, nil is returned.
+func (c *StorageBoxClient) GetByName(ctx context.Context, name string) (*StorageBox, *Response, error) {
+	return firstByName(name, func() ([]*StorageBox, *Response, error) {
+		return c.List(ctx, StorageBoxListOpts{Name: name})
+	})
+}
+
+// Get retrieves a storage box by its ID if the input can be parsed as an integer, otherwise it
+// retrieves a storage box by its name. If the storage box does not exist, nil is returned.
+func (c *StorageBoxClient) Get(ctx context.Context, idOrName string) (*StorageBox, *Response, error) {
+	return getByIDOrName(ctx, c.GetByID, c.GetByName, idOrName)
+}
+
+// StorageBoxListOpts specifies options for listing storage boxes.
+type StorageBoxListOpts struct {
+	ListOpts
+	Name string
+}
+
+func (l StorageBoxListOpts) values() url.Values {
+	vals := l.ListOpts.Values()
+	if l.Name != "" {
+		vals.Add("name", l.Name)
+	}
+	return vals
+}
+
+// List returns a list of storage boxes for a specific page.
+//
+// Please note that filters specified in opts are not taken into account
+// when their value corresponds to their zero value or when they are empty.
+func (c *StorageBoxClient) List(ctx context.Context, opts StorageBoxListOpts) ([]*StorageBox, *Response, error) {
+	const opPath = "/storage_boxes?%s"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, opts.values().Encode())
+
+	respBody, resp, err := getRequest[schema.StorageBoxListResponse](ctx, c.client, reqPath)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return allFromSchemaFunc(respBody.StorageBoxes, StorageBoxFromSchema), resp, nil
+}
+
+// All returns all storage boxes.
+func (c *StorageBoxClient) All(ctx context.Context) ([]*StorageBox, error) {
+	return c.AllWithOpts(ctx, StorageBoxListOpts{ListOpts: ListOpts{PerPage: 50}})
+}
+
+// AllWithOpts returns all storage boxes with the given options.
+func (c *StorageBoxClient) AllWithOpts(ctx context.Context, opts StorageBoxListOpts) ([]*StorageBox, error) {
+	return iterPages(func(page int) ([]*StorageBox, *Response, error) {
+		opts.Page = page
+		return c.List(ctx, opts)
+	})
+}
+
+// StorageBoxCreateOpts specifies parameters for creating a storage box.
+type StorageBoxCreateOpts struct {
+	Name           string
+	StorageBoxType *StorageBoxType
+	Location       *Location
+	Labels         map[string]string
+	Password       string
+	// TODO: API only accepts literal public keys
+	// - should we accept our structs here and read the public key?
+	// - What to do when only the ID is set and not the public key?
+	// - Should we tell users that this is not required and they can just create an object with the public key field set?
+	SSHKeys        []string
+	AccessSettings *StorageBoxCreateOptsAccessSettings
+}
+
+// StorageBoxCreateOptsAccessSettings specifies access settings for creating a storage box.
+type StorageBoxCreateOptsAccessSettings struct {
+	ReachableExternally *bool
+	SambaEnabled        *bool
+	SSHEnabled          *bool
+	WebDAVEnabled       *bool
+	ZFSEnabled          *bool
+}
+
+// StorageBoxCreateResult is the result of a create storage box operation.
+type StorageBoxCreateResult struct {
+	StorageBox *StorageBox
+	Action     *Action
+}
+
+// Create creates a new storage box with the given options.
+func (c *StorageBoxClient) Create(ctx context.Context, opts StorageBoxCreateOpts) (StorageBoxCreateResult, *Response, error) {
+	const opPath = "/storage_boxes"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqBody := SchemaFromStorageBoxCreateOpts(opts)
+
+	result := StorageBoxCreateResult{}
+
+	respBody, resp, err := postRequest[schema.StorageBoxCreateResponse](ctx, c.client, opPath, reqBody)
+	if err != nil {
+		return result, resp, err
+	}
+
+	result.StorageBox = StorageBoxFromSchema(respBody.StorageBox)
+	result.Action = ActionFromSchema(respBody.Action)
+
+	return result, resp, nil
+}
+
+// StorageBoxUpdateOpts specifies options for updating a storage box.
+type StorageBoxUpdateOpts struct {
+	Name   string
+	Labels map[string]string
+}
+
+// Update updates a storage box with the given options.
+func (c *StorageBoxClient) Update(ctx context.Context, storageBox *StorageBox, opts StorageBoxUpdateOpts) (*StorageBox, *Response, error) {
+	const opPath = "/storage_boxes/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, storageBox.ID)
+	reqBody := SchemaFromStorageBoxUpdateOpts(opts)
+
+	respBody, resp, err := putRequest[schema.StorageBoxUpdateResponse](ctx, c.client, reqPath, reqBody)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return StorageBoxFromSchema(respBody.StorageBox), resp, nil
+}
+
+// Delete deletes a storage box.
+func (c *StorageBoxClient) Delete(ctx context.Context, storageBox *StorageBox) (*Action, *Response, error) {
+	const opPath = "/storage_boxes/%d"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, storageBox.ID)
+
+	respBody, resp, err := deleteRequest[schema.ActionGetResponse](ctx, c.client, reqPath)
+	if err != nil {
+		return nil, resp, err
+	}
+	return ActionFromSchema(respBody.Action), resp, nil
+}
+
+type StorageBoxFoldersResult struct {
+	Folders []string
+}
+
+type StorageBoxFoldersOpts struct {
+	Path string
+}
+
+func (o StorageBoxFoldersOpts) values() url.Values {
+	vals := url.Values{}
+	if o.Path != "" {
+		vals.Add("path", o.Path)
+	}
+	return vals
+}
+
+// Folders lists folders in a storage box.
+func (c *StorageBoxClient) Folders(ctx context.Context, storageBox *StorageBox, opts StorageBoxFoldersOpts) (StorageBoxFoldersResult, *Response, error) {
+	const opPath = "/storage_boxes/%d/folders"
+	ctx = ctxutil.SetOpPath(ctx, opPath)
+
+	reqPath := fmt.Sprintf(opPath, storageBox.ID)
+
+	if len(opts.values()) > 0 {
+		reqPath += "?" + opts.values().Encode()
+	}
+
+	result := StorageBoxFoldersResult{}
+
+	respBody, resp, err := getRequest[schema.StorageBoxFoldersResponse](ctx, c.client, reqPath)
+	if err != nil {
+		return result, resp, err
+	}
+
+	result.Folders = respBody.Folders
+
+	return result, resp, nil
+}
