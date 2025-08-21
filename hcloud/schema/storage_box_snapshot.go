85071f6ea5a59e749551c4dd88e75a898db40c6a
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2025-08-21 14:23:55 +0200

    docs: storage box snapshot schema docs

--- hcloud/schema/storage_box_snapshot.go
+++ hcloud/schema/storage_box_snapshot.go
@@ -2,6 +2,7 @@ package schema
 
 import "time"
 
+// StorageBoxSnapshot defines the schema of a Storage Box snapshot.
 type StorageBoxSnapshot struct {
 	ID          int64                   `json:"id"`
 	Name        string                  `json:"name"`
@@ -13,34 +14,41 @@ type StorageBoxSnapshot struct {
 	StorageBox  int64                   `json:"storage_box"`
 }
 
+// StorageBoxSnapshotStats defines the schema of a Storage Box snapshot's size statistics.
 type StorageBoxSnapshotStats struct {
 	Size           uint64 `json:"size"`
 	SizeFilesystem uint64 `json:"size_filesystem"`
 }
 
+// StorageBoxSnapshotGetResponse defines the schema of the response when retrieving a single Storage Box snapshot.
 type StorageBoxSnapshotGetResponse struct {
 	Snapshot StorageBoxSnapshot `json:"snapshot"`
 }
 
+// StorageBoxSnapshotListResponse defines the schema of the response when listing Storage Box snapshots.
 type StorageBoxSnapshotListResponse struct {
 	Snapshots []StorageBoxSnapshot `json:"snapshots"`
 }
 
+// StorageBoxSnapshotCreateRequest defines the schema of the request to create a Storage Box snapshot.
 type StorageBoxSnapshotCreateRequest struct {
 	Description string `json:"description,omitempty"`
 }
 
+// StorageBoxSnapshotCreateResponse defines the schema of the response when creating a Storage Box snapshot.
 type StorageBoxSnapshotCreateResponse struct {
 	Snapshot StorageBoxSnapshot `json:"snapshot"`
 	Action   Action             `json:"action"`
 }
 
 // TODO: Both are required by the spec? Why?
+// StorageBoxSnapshotUpdateRequest defines the schema of the request to update a Storage Box snapshot.
 type StorageBoxSnapshotUpdateRequest struct {
 	Description string            `json:"description"`
 	Labels      map[string]string `json:"labels"`
 }
 
+// StorageBoxSnapshotUpdateResponse defines the schema of the response when updating a Storage Box snapshot.
 type StorageBoxSnapshotUpdateResponse struct {
 	Snapshot StorageBoxSnapshot `json:"snapshot"`
 }
